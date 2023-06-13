b688180565c86dff3a66c88c26acdb26340afa7c
paszo <paszo2008@gmail.com>
2023-06-13 08:08:20 +0200

    simplified bars expample

--- src/examples/Bars.tsx
+++ src/examples/Bars.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
-import letterFrequency, {LetterFrequency} from "@visx/mock-data/lib/mocks/letterFrequency";
+import { useMemo } from "react";
+import letterFrequency, {
+  LetterFrequency,
+} from "@visx/mock-data/lib/mocks/letterFrequency";
+import { Group } from "@visx/group";
+import { scaleBand, scaleLinear } from "@visx/scale";
+import { Bar } from "@visx/shape";
 
-const data = letterFrequency.slice(0,5);
+const data = letterFrequency.slice(0, 5);
 const verticalMargin = 120;
 
 console.log(data);
@@ -10,23 +15,60 @@ console.log(data);
 const getLetter = (d: LetterFrequency) => d.letter;
 const getLetterFrequency = (d: LetterFrequency) => Number(d.frequency) * 100;
 
-
 export type BarsProps = {
-    width: number;
-    height: number;
-    events?: boolean;
-}
-const Bars = ({width, height, events = false}: BarsProps) => {
+  width: number;
+  height: number;
+};
+const Bars = ({ width, height }: BarsProps) => {
+  const xMax = width;
+  const yMax = height - verticalMargin;
 
+  const xScale = useMemo(
+    () =>
+      scaleBand<string>({
+        range: [0, xMax],
+        domain: data.map(getLetter),
+        padding: 0.4,
+      }),
+    [xMax]
+  );
 
+  const yScale = useMemo(
+    () =>
+      scaleLinear<number>({
+        range: [yMax, 0],
+        domain: [0, Math.max(...data.map(getLetterFrequency))],
+      }),
+    [yMax]
+  );
 
-
-
-    return (
-        <svg width={width} height={height}>
-
-        </svg>
-    );
+  return (
+    <svg width={width} height={height}>
+      <rect width={width} height={height} fill="#f1f1f1" />
+      <Group top={verticalMargin / 2}>
+        {data.map((d) => {
+          const letter = getLetter(d);
+          const barWidth = xScale.bandwidth();
+          const barHeight = yMax - yScale(getLetterFrequency(d));
+          const barX = xScale(letter);
+          const barY = yMax - barHeight;
+          return (
+            <Bar
+              key={`bar-${letter}`}
+              x={barX}
+              y={barY}
+              width={barWidth}
+              height={barHeight}
+              fill={"green"}
+              onClick={() => {
+                console.log(`clicked : ${JSON.stringify(Object.values(d))}`);
+              }}
+            />
+          );
+        })}
+      </Group>
+    </svg>
+  );
 };
 
 export default Bars;
