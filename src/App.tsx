9ba83c2208cd1fc9d84f51eaddb5b0a4a57289d1
paszo <paszo2008@gmail.com>
2023-06-13 06:59:12 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Bars from  "./examples/Bars";
+
+function App() {
+  return (
+    <div className="App">
+      <Bars/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/examples/Bars.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Bars = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Bars;
